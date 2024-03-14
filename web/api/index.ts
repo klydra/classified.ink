543492732f95de4490a66d462db2f2acbd8278a8
Jan Klinge <jan@klydra.dev>
2024-03-14 18:45:02 +0100

    ran prettier

--- web/api/index.ts
+++ web/api/index.ts
@@ -1,7 +1,5 @@
-import {Elysia} from 'elysia'
+import { Elysia } from "elysia";
 
-const app = new Elysia()
-    .get('/', () => 'Hello Elysia')
-    .listen(8080)
+const app = new Elysia().get("/", () => "Hello Elysia").listen(8080);
 
-console.log(`🦊 Elysia is running at on port ${app.server?.port}...`)
+console.log(`🦊 Elysia is running at on port ${app.server?.port}...`);
