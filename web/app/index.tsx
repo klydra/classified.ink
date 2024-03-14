36f5634d52f2c073743e27a3e3cf793dd169b9b4
Jan Klinge <jan@klydra.dev>
2024-03-14 16:56:30 +0100

    init vite

--- web/app/index.tsx
+++ web/app/index.tsx
@@ -1,3 +1,12 @@
-export default function App() {
-    return <div></div>;
-}
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import '@/app/globals.css'
+import {Button} from "@/components/ui/button.tsx";
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <Button>
+            test
+        </Button>
+    </React.StrictMode>,
+)
