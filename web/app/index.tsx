1bec34438bcbbb8c7dd277f0a41d0d12bcfa2c1c
Jan Klinge <jan@klydra.dev>
2024-03-14 18:39:30 +0100

    Structuring

--- web/app/index.tsx
+++ web/app/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import '@/app/globals.css'
-import Home from "@/app/pages/home/home.tsx";
-import {ThemeProvider} from "@/components/theme-provider.tsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@/app/globals.css";
+import Home from "@/pages/home/home.tsx";
+import { ThemeProvider } from "@/components/theme-provider.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
-      <Home/>
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
-)
+);
