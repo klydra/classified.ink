e4c0e7c1bbf7274c2a8ec9f380a2daaa0f6a2c9c
Jan Klinge <jan@klydra.dev>
2024-03-20 21:34:12 +0100

    delete test route + clear about

--- web/pages/about.tsx
+++ web/pages/about.tsx
@@ -3,9 +3,5 @@ import { createFileRoute } from "@tanstack/react-router";
 export const Route = createFileRoute("/about")({ component: Component });
 
 function Component() {
-  return (
-    <div className="p-2">
-      <h3>Welcome to About! 3</h3>
-    </div>
-  );
+  return <div></div>;
 }
