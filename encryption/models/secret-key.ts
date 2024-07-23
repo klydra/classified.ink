903d9fbf62a4a277c23883ec21b6db5906981ffa
Jan Klinge <jan@klydra.dev>
2024-07-23 17:31:01 +0200

    Remove passport

--- encryption/models/secret-key.ts
+++ encryption/models/secret-key.ts
@@ -1,24 +1,18 @@
-import { decryptIV, encryptIV, pbkdf2, randomBytes } from "../crypto.ts";
-import SecretLock, { SECRET_LOCK_DERIVED_LENGTH } from "./secret-lock.ts";
+import { decryptIV, encryptIV, randomBytes } from "../crypto.ts";
+import SecretLock from "./secret-lock.ts";
 
 export const SECRET_KEY_IV_LENGTH = 16;
-export const SECRET_KEY_PASSPORT_LENGTH = SECRET_LOCK_DERIVED_LENGTH;
-export const SECRET_KEY_PASSPORT_ITERATIONS = 600000;
 export const SECRET_KEY_BLOB_LENGTH = 32;
 export const SECRET_KEY_BLOB_ENCRYPTED_LENGTH = 80;
 export const SECRET_KEY_LENGTH =
-  SECRET_KEY_IV_LENGTH +
-  SECRET_KEY_PASSPORT_LENGTH +
-  SECRET_KEY_BLOB_ENCRYPTED_LENGTH;
+  SECRET_KEY_IV_LENGTH + SECRET_KEY_BLOB_ENCRYPTED_LENGTH;
 
 export default class SecretKey {
   iv: string;
-  passport: string;
   blob: string;
 
-  constructor(iv: string, passport: string, blob: string) {
+  constructor(iv: string, blob: string) {
     this.iv = iv;
-    this.passport = passport;
     this.blob = blob;
 
     console.log("Created SecretKey " + this.describe());
@@ -27,13 +21,7 @@ export default class SecretKey {
   static fromString(secretKey: string): SecretKey {
     return new SecretKey(
       secretKey.slice(0, SECRET_KEY_IV_LENGTH * 2),
-      secretKey.slice(
-        SECRET_KEY_IV_LENGTH * 2,
-        SECRET_KEY_IV_LENGTH * 2 + SECRET_KEY_PASSPORT_LENGTH * 2,
-      ),
-      secretKey.slice(
-        SECRET_KEY_IV_LENGTH * 2 + SECRET_KEY_PASSPORT_LENGTH * 2,
-      ),
+      secretKey.slice(SECRET_KEY_IV_LENGTH * 2),
     );
   }
 
@@ -49,44 +37,15 @@ export default class SecretKey {
 
     const encrypted = await encryptIV(derived, iv, secretKey);
 
-    console.log("Generating Passport...");
-
-    const passport = await pbkdf2(
-      encrypted,
-      derived,
-      SECRET_KEY_PASSPORT_ITERATIONS,
-      SECRET_KEY_PASSPORT_LENGTH,
-    );
-
-    console.log(
-      "Generating SecretKey [iv=" +
-        iv +
-        ", derived=" +
-        derived +
-        " -> passport=" +
-        passport +
-        ", blob=" +
-        encrypted +
-        "]",
-    );
-
-    return new SecretKey(iv, passport, encrypted);
+    return new SecretKey(iv, encrypted);
   }
 
   toString(): string {
-    return this.iv + this.passport + this.blob;
+    return this.iv + this.blob;
   }
 
   describe(): string {
-    return (
-      "[iv=" +
-      this.iv +
-      ", passport=" +
-      this.passport +
-      ", blob=" +
-      this.blob +
-      "]"
-    );
+    return "[iv=" + this.iv + ", blob=" + this.blob + "]";
   }
 
   async decrypt(lock: SecretLock): Promise<string> {
@@ -111,31 +70,6 @@ export default class SecretKey {
   async proof(lock: SecretLock): Promise<boolean> {
     console.log("Generating Proof for SecretKey...");
 
-    const derived = await lock.derive();
-    const decrypted = await this.decrypt(lock);
-
-    console.log("Encrypting SecretKey...");
-
-    const encrypted = await encryptIV(derived, this.iv, decrypted);
-
-    console.log("Generating Proof to match against...");
-
-    const expected = await pbkdf2(
-      encrypted,
-      derived,
-      SECRET_KEY_PASSPORT_ITERATIONS,
-      SECRET_KEY_PASSPORT_LENGTH,
-    );
-
-    console.log(
-      "Generated Proof for SecretKey " +
-        this.describe() +
-        " and derived lock " +
-        derived +
-        " with result " +
-        expected,
-    );
-
-    return expected === this.passport;
+    return false;
   }
 }
