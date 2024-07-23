66922d5723822437420206069146ad36df77a7ba
Jan Klinge <jan@klydra.dev>
2024-07-23 19:44:51 +0200

    Use grid

--- web/pages/hello.tsx
+++ web/pages/hello.tsx
@@ -9,25 +9,15 @@ export const Route = createFileRoute("/hello")({
     const phrase = userStore.pk
       ? bitsToBIP39(uint8ArrayToBits(hexStringToUint8Array(userStore.pk)))
       : undefined;
-    const delimiter = phrase ? phrase.length / 3 : undefined;
 
     return (
       <div className="flex-grow w-full flex flex-col justify-center items-center">
         <p className="px-4 py-6 text-2xl">
           Welcome aboard, <b>{userStore.username}</b>!
         </p>
-        <div className="px-4 py-12 text-2xl font-bold leading-relaxed">
+        <div className="grid grid-cols-4 gap-x-8 gap-y-4 px-4 py-16 text-2xl text-center font-bold leading-relaxed">
           {phrase?.map((word, index) => {
-            return (
-              <span key={index} className="mx-2">
-                {word}
-                {delimiter &&
-                index % delimiter === delimiter - 1 &&
-                index !== phrase.length - 1 ? (
-                  <br />
-                ) : undefined}
-              </span>
-            );
+            return <p key={index}>{word}</p>;
           })}
         </div>
         <p>
